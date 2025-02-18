559e87e15c9578b7dad3335c2a61c3946e860821
Deima.Balc <95541317+DeimaBalc@users.noreply.github.com>
2025-02-18 19:45:39 +0200

    Update geometrija.cpp

--- geometrija.cpp
+++ geometrija.cpp
@@ -2,50 +2,87 @@
 #include <iostream>
 #include <cmath>
 
-double atstumasIkiKoordinaciuPradzios(const Taskas3D& taskas){
-      return sqrt(taskas.x * taskas.x + taskas.y * taskas.y + taskas.z * taskas.z);
+Taskas3D::Taskas3D() : x(0), y(0), z(0) {}
+
+Taskas3D::Taskas3D(double x, double y, double z){
+
+    this-> x = x;
+    this-> y = y;
+    this-> z = z; 
+
 }
 
-double atstumasTarpTasku(const Taskas3D& taskas1, const Taskas3D& taskas2){
+Atkarpa3D::Atkarpa3D(const Taskas3D& a, const Taskas3D& b){
+
+    this-> a = a;
+    this-> b = b;
+
+}
+
+Trikampis3D::Trikampis3D(const Taskas3D& a, const Taskas3D& b, const Taskas3D& c){
+
+    this-> a = a;
+    this-> b = b;
+    this-> c = c;
+
+}
+
+double Taskas3D::atstumasIkiKoordinaciuPradzios(const Taskas3D& taskas){
+      
+      return sqrt(taskas.x * taskas.x + taskas.y * taskas.y + taskas.z * taskas.z);
+      
+}
+
+double Taskas3D::atstumasTarpTasku(const Taskas3D& taskas1, const Taskas3D& taskas2){
+      
       double dx = taskas2.x - taskas1.x;
       double dy = taskas2.y - taskas1.y;
       double dz = taskas2.z - taskas1.z;
  
       return sqrt(dx * dx + dy * dy + dz * dz);
+
 }
 
-double atkarposIlgis(const Atkarpa3D& atkarpa){
-     return atstumasTarpTasku(atkarpa.a, atkarpa.b);
+double Atkarpa3D::atkarposIlgis(const Atkarpa3D& atkarpa){
+     
+     return Taskas3D::atstumasTarpTasku(atkarpa.a, atkarpa.b);
+
 }
 
-void skaitykTaska(Taskas3D& taskas){
+void Taskas3D::skaitykTaska(Taskas3D& taskas){
 
     std::cin >> taskas.x >> taskas.y >> taskas.z;
 
 }
 
-void rasykTaska(const Taskas3D& taskas){
+void Taskas3D::rasykTaska(const Taskas3D& taskas){
     std::cout << "(" << taskas.x << ", " <<  taskas.y << ", " << taskas.z << ")";
-  //      std::cout
-  
+    
 }
 
-double trikampioPlotas(const Trikampis3D& trikampis){
-    double ac=atstumasTarpTasku(trikampis.a, trikampis.c);
-    double ab=atstumasTarpTasku(trikampis.a, trikampis.b);
-    double bc=atstumasTarpTasku(trikampis.b, trikampis.c);
+double Trikampis3D::trikampioPlotas(const Trikampis3D& trikampis){
+    
+    double ac=Taskas3D::atstumasTarpTasku(trikampis.a, trikampis.c);
+    double ab=Taskas3D::atstumasTarpTasku(trikampis.a, trikampis.b);
+    double bc=Taskas3D::atstumasTarpTasku(trikampis.b, trikampis.c);
+    
     double p=(ac+ab+bc)/2;
     double plotas=sqrt(p*(p-ac)*(p-ab)*(p-bc));
+    
     return plotas;
+
 }
-double trikampioPerimetras(const Trikampis3D& trikampis){
-    double ac=atstumasTarpTasku(trikampis.a, trikampis.c);
-    double ab=atstumasTarpTasku(trikampis.a, trikampis.b);
-    double bc=atstumasTarpTasku(trikampis.b, trikampis.c);
+
+double Trikampis3D::trikampioPerimetras(const Trikampis3D& trikampis){
+    
+    double ac=Taskas3D::atstumasTarpTasku(trikampis.a, trikampis.c);
+    double ab=Taskas3D::atstumasTarpTasku(trikampis.a, trikampis.b);
+    double bc=Taskas3D::atstumasTarpTasku(trikampis.b, trikampis.c);
+    
     double perimetras=(ac+ab+bc);
+    
     return perimetras;
 
 }
-
 
 
