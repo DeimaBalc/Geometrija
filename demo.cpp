47066193c42652a2ce4481a68f9b0f6441867972
Deima.Balc <95541317+DeimaBalc@users.noreply.github.com>
2025-02-18 19:45:04 +0200

    asdfgh

--- demo.cpp
+++ demo.cpp
@@ -5,28 +5,35 @@ int main(){
 
     Taskas3D pA{0,0,0};
     Taskas3D pB{1, 0, 0};
-    Taskas3D pC{0, 2, 0};
-    Taskas3D pD{0, 0, 2};
+    Taskas3D pC{0, 1, 0};
+    Taskas3D pD{0, 0, 1};
+
+    //Tasku skaitymas
     
-    //skaitykTaska(pB);
-    std::cout << "Taskas pA: "; 
-    rasykTaska(pA);
-    std::cout << std::endl;
-    std::cout << "Taskas pB: "; 
-    rasykTaska(pB);
-    std::cout << std::endl;
+    std::cout<<"Įvesktite tašką pA: "<<std::endl;
+    pA.skaitykTaska(pA);
+    std::cout << "Taskas pA: ";
+    pA.rasykTaska(pA);
+    std::cout<< std::endl; 
+
+    std::cout<<"Įvesktite tašką pB: "<<std::endl;
+    pB.skaitykTaska(pB);
+    std::cout << "Taskas pB: ";
+    pB.rasykTaska(pB);
+    std::cout<< std::endl;
     
-    std::cout << "Atstumas nuo pB iki k. centro: " << atstumasIkiKoordinaciuPradzios(pB) << std::endl; 
+    std::cout << "Atstumas nuo pB iki k. centro: " << pB.atstumasIkiKoordinaciuPradzios(pB) << std::endl; 
     Atkarpa3D atkAB{pA, pB};
-    std::cout << "Atkarpos AB ilgis: " << atkarposIlgis(atkAB) << std::endl;
+    std::cout << "Atkarpos AB ilgis: " << atkAB.atkarposIlgis(atkAB) << std::endl;
+
     Trikampis3D trkABC{pA, pB, pC};
-    std::cout << "Trikampio ABC plotas: " << trikampioPlotas(trkABC) << std::endl;
-    std::cout << "Trikampio ABC perimetras: " << trikampioPerimetras(trkABC) << std::endl;
+    std::cout << "Trikampio ABC plotas: " << trkABC.trikampioPlotas(trkABC) << std::endl;
+    std::cout << "Trikampio ABC perimetras: " << trkABC.trikampioPerimetras(trkABC) << std::endl;
     
     Trikampis3D trkBCD{pC, pB, pD};
-    std::cout << "Trikampio BCD plotas: " << trikampioPlotas(trkBCD) << std::endl;
-    std::cout << "Trikampio BCD perimetras: " << trikampioPerimetras(trkBCD) << std::endl;
-    
+    std::cout << "Trikampio BCD plotas: " << trkBCD.trikampioPlotas(trkBCD) << std::endl;
+    std::cout << "Trikampio BCD perimetras: " << trkBCD.trikampioPerimetras(trkBCD) << std::endl;
+   
     return 0;
 
 
