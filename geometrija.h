c06bdc43a2cedefcec736f02b014babbd02617ae
Deima.Balc <95541317+DeimaBalc@users.noreply.github.com>
2025-02-18 19:46:32 +0200

    Update geometrija.h

--- geometrija.h
+++ geometrija.h
@@ -1,34 +1,39 @@
 #ifndef _GEOMETRIJA3D_H_
 #define _GEOMETRIJA3D_H_
 
-
-struct Taskas3D{
-   double x;
-   double y;
-   double z;
-};
-
-struct Atkarpa3D{
-   Taskas3D a;
-   Taskas3D b;
-};
-
-struct Trikampis3D{
-   Taskas3D a;
-   Taskas3D b;
-   Taskas3D c;
+class Taskas3D{
+   private:
+      double x;
+      double y;
+      double z;
+   public:
+      Taskas3D();
+      Taskas3D(const double x, const double y, const double z);
+      double atstumasIkiKoordinaciuPradzios(const Taskas3D& taskas);
+      static double atstumasTarpTasku(const Taskas3D& taskas1, const Taskas3D& taskas2);
+      static void skaitykTaska(Taskas3D& taskas);
+      void rasykTaska(const Taskas3D& taskas);
 };
 
 
-double atstumasIkiKoordinaciuPradzios(const Taskas3D& taskas);
-double atstumasTarpTasku(const Taskas3D& taskas1, const Taskas3D& taskas2);
-double atkarposIlgis(const Atkarpa3D& atkarpa);
-void skaitykTaska(Taskas3D& taskas);
-void rasykTaska(const Taskas3D& taskas);
-double trikampioPlotas(const Trikampis3D& trikampis);
+class Atkarpa3D{
+   private:
+      Taskas3D a;
+      Taskas3D b;
+   public:
+      Atkarpa3D(const Taskas3D& a, const Taskas3D& b);
+      double atkarposIlgis(const Atkarpa3D& atkarpa);
+};
 
-double trikampioPerimetras(const Trikampis3D& trikampis);
-
-
+class Trikampis3D{
+   private:
+      Taskas3D a;
+      Taskas3D b;
+      Taskas3D c;
+   public:
+      Trikampis3D(const Taskas3D& a, const Taskas3D& b, const Taskas3D& c);
+      double trikampioPlotas(const Trikampis3D& trikampis);
+      double trikampioPerimetras(const Trikampis3D& trikampis);
+};
 
 #endif
